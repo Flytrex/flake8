ec9182ba24350f7acaf0e3e5ba3cbe87f17f286c
Apakottur <54272821+Apakottur@users.noreply.github.com>
2020-08-27 14:05:05 +0000

    Bump Flytrex version

--- src/flake8/__init__.py
+++ src/flake8/__init__.py
@@ -21,7 +21,7 @@ LOG.addHandler(logging.NullHandler())
 
 def flytrex_version(version):
     delimiter = "." if "+" in version else "+"
-    return f"{version}{delimiter}flytrex-2"
+    return f"{version}{delimiter}flytrex-3"
 
 
 __version__ = flytrex_version("3.8.3")
